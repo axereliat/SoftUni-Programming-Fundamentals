73df327fa42775ec8abdd1ce45ff9a595805f84f
axereliat <axereliat@gmail.com>
2017-02-03 20:48:30 +0200

    Initial commit
    Signed-off-by: axereliat <axereliat@gmail.com>

---
+++ Arrays - Exercises/01. Largest Common End/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.Largest_Common_End
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] s1 = Console.ReadLine().Split(' ');
+            string[] s2 = Console.ReadLine().Split(' ');
+
+            int cnt = 0;
+            
+            for (int i = 0; i < Math.Min(s1.Length, s2.Length); i++)
+            {
+                if (s1[i] == s2[i]) {
+                    cnt++;
+                }
+            }
+            int cnt2 = 0;
+            Array.Reverse(s1);
+            Array.Reverse(s2);
+            for (int i = 0; i < Math.Min(s1.Length, s2.Length); i++)
+            {
+                if (s1[i] == s2[i])
+                {
+                    cnt2++;
+                }
+            }
+            Console.WriteLine(Math.Max(cnt, cnt2));
+        }
+    }
+}
---
+++ Arrays - Exercises/02. Rotate and Sum/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02.Rotate_and_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Console
+                .ReadLine()
+                .Trim()
+                .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(x => int.Parse(x))
+                .ToArray();
+            int k = int.Parse(Console.ReadLine());
+            int rotations = new int[k][10];
+
+            for (int i = 0; i < k; i++)
+            {
+                for (int j = 0; j < arr.Length; j++)
+                {
+                    Console.WriteLine(arr[i] * 2);
+                }
+            }
+        }
+    }
+}
